28d0443ac488a8adfcc20b25d51747dfff8a16ed
imranalibrohi148 <imranalibrohi148@gmail.com>
2026-09-02 22:32:23 +0500

    add the chnages

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Support\ContentDrip;
 use App\Models\{
+    CourseCategory,
+    CourseLevel,
     Certificate,
     QuizAttempt,
     Enrollment,
